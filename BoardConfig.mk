2fd2a6997cb57549896685632492243e7ab0b010
Franco <frapeti@gmail.com>
2013-05-01 17:17:20 -0300

    device specific headers path added
    override anything from 'android_device_samsung_u8500-common'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_PROVIDES_INIT := true
 TARGET_PROVIDES_INIT_TARGET_RC := true
 TARGET_RECOVERY_INITRC := device/samsung/janice/rootdir/recovery.rc
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/janice/overlay/include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/janice/include
 
 # Kernel
 BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200"
