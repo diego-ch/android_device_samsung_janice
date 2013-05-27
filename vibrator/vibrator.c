20bf8fdaedbceb2d3f7686178ba60292cb3edb5d
Franco Rapetti <frapeti@hotmail.com>
2013-05-27 15:57:23 -0300

    vibrator: new fix test

--- vibrator/vibrator.c
+++ vibrator/vibrator.c
@@ -21,18 +21,28 @@
 #include <fcntl.h>
 #include <errno.h>
 
+
+#ifdef USE_ALTERNATIVE_VIBRATOR
+extern int sendit(int timeout_ms);
+#else
+
 #define THE_DEVICE "/sys/class/timed_output/vibrator/enable"
+
+int vibrator_exists()
+{
+    int fd;
+
+    fd = open(THE_DEVICE, O_RDWR);
+    if(fd != 0)
+        return 1;
+    close(fd);
+    return 0;
+}
 
 static int sendit(int timeout_ms)
 {
     int nwr, ret, fd;
     char value[20];
-
-#ifdef QEMU_HARDWARE
-    if (qemu_check()) {
-        return qemu_control_command( "vibrator:%d", timeout_ms );
-    }
-#endif
 
     fd = open(THE_DEVICE, O_RDWR);
     if(fd < 0)
@@ -46,13 +56,19 @@ static int sendit(int timeout_ms)
     return (ret == nwr) ? 0 : -1;
 }
 
+
 int vibrator_on(int timeout_ms)
 {
     /* constant on, up to maximum allowed time */
-    return sendit(timeout_ms);
+	if(timeout_ms < 0)
+		return sendit(5000);
+	return sendit(timeout_ms);
 }
 
 int vibrator_off()
-{
-    return sendit(0);
+{	
+	/* checks if the device is vibrating, if it's vibrating stop, if not, do nothing */
+	return (vibrator_exists() == 0) ? 0 : 1;
 }
+
+#endif
