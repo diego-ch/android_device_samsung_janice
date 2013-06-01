b0f357edcb90b394b98f16cc7a967f3e3f77a095
Shantanu Londhe <shantanulondhe26@gmail.com>
2013-06-01 17:50:15 +0530

    Mic fix testing
    Test for microphone to work universally. Please use https://github.com/Shaaan/android_hardware_libhardware/blob/jellybean/include/hardware/audio.h

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 #COMMON_GLOBAL_CFLAGS += -DSTE_AUDIO
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_STE_SUDIO_BLOB
 
 # Graphics
 BOARD_EGL_CFG := device/samsung/janice/configs/egl.cfg
