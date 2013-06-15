5ce806cfc07393128edb2d739f23365c5657913e
Diego Porto <diego.cfporto@gmail.com>
2013-06-14 22:07:41 -0300

    move main BoardConfig to common repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,121 +1,15 @@
-USE_CAMERA_STUB := false
-BOARD_USES_GENERIC_AUDIO := false
+-include device/samsung/galaxys2-common/BoardCommonConfig.mk
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/janice/include
 
-# Architecture
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
-ARCH_ARM_HAVE_NEON := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-
-# Platform
-TARGET_BOARD_PLATFORM := montblanc
-TARGET_SOC := u8500
-TARGET_BOOTLOADER_BOARD_NAME := montblanc
-BOARD_USES_STE_HARDWARE := true
-COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE
-
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
-TARGET_RECOVERY_INITRC := device/samsung/janice/rootdir/recovery.rc
+# Vibrator
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/janice/vibrator/vibrator.c
 
-# Flags
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-
 # Kernel
-BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200"
-BOARD_KERNEL_BASE := 0x40000000
-BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
 TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
 
-# Filesystem
-BOARD_NAND_PAGE_SIZE := 4096
-BOARD_NAND_SPARE_SIZE := 128
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 641728512
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE := 4096
-TARGET_USERIMAGES_USE_EXT4 := true
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-COMMON_GLOBAL_CFLAGS += -DSTE_AUDIO
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_STE_AUDIO_BLOB
-
-# Camera
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_STE
-
-# Graphics
-BOARD_EGL_CFG := device/samsung/janice/configs/egl.cfg
-USE_OPENGL_RENDERER := true
-
-# Enable WEBGL in WebKit
-ENABLE_WEBGL := true
-
-# HWComposer
-BOARD_USES_HWCOMPOSER := true
-
-# RIL
-BOARD_USES_LIBSECRIL_STUB := true
-BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
-
-# Wifi
-BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_WLAN_DEVICE_REV            := bcm4330
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
-WIFI_DRIVER_MODULE_NAME          := "dhd"
-WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_MODULE_AP_ARG        := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_BAND                        := 802_11_ABG
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 12
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun%d/file"
-
-# Recovery
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/janice/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/janice/recovery/graphics.c
-BOARD_UMS_LUNFILE := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun0/file"
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-
-# Charging mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := "battery"
-BOARD_CHARGER_RES := device/samsung/janice/res/charger
-
-# Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/janice/releasetools/janice_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/samsung/janice/releasetools/janice_img_from_target_files
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/janice/shbootimg.mk
-
+# Assert
 TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070
 
 # Inherit from the proprietary version
