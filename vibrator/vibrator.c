1b7de77be3f09fc4cb09f0768ac309c355167ec9
Pablo Lunarenko <kimiaropablo958@gmail.com>
2013-06-19 23:38:34 -0300

    Delete including of hardware_legacy/vibrator.c
    We have everything defined in the .c file, we don't want to include vibrator from legacy because it will complain about the function being defined in other way

--- vibrator/vibrator.c
+++ vibrator/vibrator.c
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <hardware_legacy/vibrator.h>
 #include "qemu.h"
 
 #include <stdio.h>
