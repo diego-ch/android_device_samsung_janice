ce9fd9c1f4509b58ab8cd9b9be4e01fbd66b0f34
Franco Rapetti <frapeti@gmail.com>
2013-07-17 00:34:19 -0300

    Update ChargerCurrency.java

--- DeviceSettings/src/com/cyanogenmod/settings/device/ChargerCurrency.java
+++ DeviceSettings/src/com/cyanogenmod/settings/device/ChargerCurrency.java
@@ -31,7 +31,7 @@ public class ChargerCurrency extends ListPreference implements OnPreferenceChang
         this.setOnPreferenceChangeListener(this);
     }
 
-    private static final String FILE = "/sys/module/abb_charger/parameters/max_ac_current";
+    private static final String FILE = "/sys/kernel/abb_charger/max_ac_c";
 
     public static boolean isSupported() {
         return Utils.fileExists(FILE);
