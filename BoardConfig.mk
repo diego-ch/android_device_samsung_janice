53c3fd3d3a3c2a3c3bfd591897fb22b27b5f59fa
OliverG96 <oliverarafo@gmail.com>
2013-08-01 20:35:09 +0100

    board: Clean-up
    Revert if you don't like it

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,20 @@
 -include device/samsung/u8500-common/BoardCommonConfig.mk
 
+TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070
+
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := montblanc
 TARGET_BOARD_INFO_FILE := device/samsung/janice/board-info.txt
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/janice/bluetooth
+# Platform 
+TARGET_BOARD_PLATFORM := montblanc
+BOARD_USES_STE_HARDWARE := true
+TARGET_SOC := u8500
+COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE -DSTE_SAMSUNG_HARDWARE
 
-
-BOARD_RIL_CLASS := ../../../device/samsung/janice/ril/
-
-# Kernel
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_BASE := 0x40000000
-BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200 consoleblank=0"
-
-# Filesystem
+# Partitions
 BOARD_NAND_PAGE_SIZE := 4096
 BOARD_NAND_SPARE_SIZE := 128
 BOARD_FLASH_BLOCK_SIZE := 4096
@@ -22,15 +23,20 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 641728512
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/u8500
+TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_CMDLINE := "console=ttySAC2,115200 consoleblank=0"
 
-# Experimental testing for flickering
+# Graphics
 BOARD_EGL_NEEDS_LEGACY_FB := true
 TARGET_DISABLE_TRIPLE_BUFFERING := true
 COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
 
-
-# EXPERIMENTAL SOUND
-# BOARD_USE_SAMSUNG_SEPARATEDSTREAM := true
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/janice/bluetooth
 
 # Wifi
 BOARD_WLAN_DEVICE                := bcmdhd
@@ -52,40 +58,18 @@ WIFI_BAND                        := 802_11_ABG
 BOARD_LEGACY_NL80211_STA_EVENTS  := true
 BOARD_HAVE_SAMSUNG_WIFI := true
 
-# Platform
-TARGET_SOC := u8500
-TARGET_BOARD_PLATFORM := montblanc
-TARGET_BOOTLOADER_BOARD_NAME := montblanc
-COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE
-
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
-
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/janice/ril/
 
 # Vibrator
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/janice/vibrator/vibrator.c
-
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/u8500
-TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
-
 
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 12
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 
-
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/janice/recovery/recovery_keys.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/janice/recovery/graphics.c
 TARGET_RECOVERY_INITRC := device/samsung/janice/rootdir/recovery.rc
-# Assert
-TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070
-
-
-# Inherit from the proprietary version
--include vendor/samsung/u8500-common/BoardConfigVendor.mk
