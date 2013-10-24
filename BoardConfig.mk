c50fdb8980ce39ecb68d2f3d05233ef9b4f47e41
Franco Rapetti <frapeti@gmail.com>
2013-10-24 00:11:13 -0200

    overwritting RECOVERY_FSTAB_VERSION since we are using legacy fstab yet...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,3 +14,4 @@ BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/janice/vibrator/vibrat
 
 # Recovery
 TARGET_RECOVERY_INITRC := device/samsung/janice/rootdir/recovery.rc
+RECOVERY_FSTAB_VERSION := 1
