3074b13a3be1386f5ba18eb2bc0b739fd66ea22d
Robert Rozic <rooobertinhooo@gmail.com>
2014-03-13 20:11:54 +0100

    Sirfgps is janice specific

--- janice.mk
+++ janice.mk
@@ -26,10 +26,11 @@ PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
     org.cyanogenmod.hardware.xml
 
-# Gps
+# GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
-    
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(COMMON_PATH)/configs/sirfgps.conf:system/etc/sirfgps.conf
+
 # Compass workaround
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/compass:system/etc/init.d/compass
