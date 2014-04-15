71f2ec9feb609d97f73df452cc26db0dd67103ef
t4n017 <tanoabeleyra@gmail.com>
2014-04-14 23:19:57 -0300

    Use ARM_EABI_TOOLCHAIN variable
    Use ARM_EABI_TOOLCHAIN variable to avoid editing toolchain path on build/core/task/kernel.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,8 @@ TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
 TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+ARM_EABI_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/janice/bluetooth
