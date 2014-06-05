5ddde6cc803c14ba99f6fe0f8093ec223e9e1afd
Robert Rozic <rooobertinhooo@gmail.com>
2014-06-05 22:08:30 +0200

    Move more lines to common init
    +Make common usb init

--- janice.mk
+++ janice.mk
@@ -27,10 +27,9 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 # Init files
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/device_tunables.rc:root/device_tunables.rc \
     $(LOCAL_PATH)/rootdir/fstab.samsungjanice:root/fstab.samsungjanice \
     $(LOCAL_PATH)/rootdir/init.samsungjanice.rc:root/init.samsungjanice.rc \
-    $(LOCAL_PATH)/rootdir/device_tunables.rc:root/device_tunables.rc \
-    $(LOCAL_PATH)/rootdir/init.samsungjanice.usb.rc:root/init.samsungjanice.usb.rc \
     $(LOCAL_PATH)/rootdir/init.recovery.samsungjanice.rc:root/init.recovery.samsungjanice.rc \
     $(LOCAL_PATH)/rootdir/ueventd.samsungjanice.rc:root/ueventd.samsungjanice.rc
 
