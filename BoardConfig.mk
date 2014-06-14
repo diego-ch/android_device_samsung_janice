addcb2efa56221dbcd892c81adf00d8262883ba6
Pablo Lunarenko <kimiaropablo958@gmail.com>
2014-06-14 10:09:46 -0300

    Initial SELinux for janice
    Very basic.
    TODO: fix SELinux policies and change to Enforcing mode
    
    Change-Id: Ibc61752d359c7564ed487a0f6b9a238aa90ca43d
    Reminder: Kill Rox.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,8 +5,6 @@ TARGET_OTA_ASSERT_DEVICE := janice,i9070,GT-I9070
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
 TARGET_KERNEL_CONFIG := cyanogenmod_i9070_defconfig
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
-ARM_EABI_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/janice/bluetooth
@@ -19,6 +17,22 @@ TARGET_RECOVERY_FSTAB := device/samsung/janice/rootdir/fstab.samsungjanice
 
 # Hardware tunables (device parts replacement)
 BOARD_HARDWARE_CLASS := device/samsung/janice/cmhw
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/janice/selinux
+
+BOARD_SEPOLICY_UNION += \
+    device.te \
+    file.te \
+    rild.te \
+    drmserver.te \
+    ueventd.te \
+    domain.te \
+    system.te \
+    file_contexts \
+    wpa_supplicant.te \
+    vold.te
 
 # Disable legacy sensors using because janice has gyro
 BOARD_USE_LEGACY_SENSORS_FUSION := false
